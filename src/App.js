70ef25142cfe8c0510714a53209d6a8202785658
dkanderson <dka.rainmaking@yahoo.com>
2019-12-12 20:12:31 -0500

    Add click handler and toggle logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavButtons from './components/nav_buttons';
 import Back from './components/back';
 import Calculator from './components/calculator';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      extended: false
+    }
+    this.toggleWidth = this.toggleWidth.bind(this);
+  }
+
+  toggleWidth(){
+    this.setState({
+      extended: !this.state.extended
+    })
+  }
+
+  render(){
+
+    return (
     
-    <div id="wrapper" className="flipWrap">
-      <NavButtons/>
-      <div id="calculator-flipper" className="flipthis">
-        <Calculator />
-        <Back />
+      <div id="wrapper" className={`flipWrap ${this.state.extended ? 'ext' : 'cmp'}`}>
+        <NavButtons toggle={this.toggleWidth} />
+        <div id="calculator-flipper" className="flipthis">
+          <Calculator ext={this.state.extended}/>
+          <Back />
+        </div>
       </div>
-    </div>
+    
+    );
+
+  }
   
-  );
 }
 
 export default App;
