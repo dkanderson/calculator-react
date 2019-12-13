363b870b8da10ef613f17fd3f1fae4c2e25807c5
dkanderson <dka.rainmaking@yahoo.com>
2019-12-12 20:17:34 -0500

    Add logic and styles for toggling memory and radian indicators

--- src/components/screen.js
+++ src/components/screen.js
@@ -3,8 +3,17 @@ import React from 'react';
 function Screen(props){
     return(
         <section className="screen">
-        <span id="display-text" className="display-text" data-result>{props.content}</span>
-        <div className="display gradient"></div>
+            { (props.rad && props.ext) && 
+                <span className="rad-display">Rad</span> }
+
+            { (props.ext && props.mem) && 
+                <span className="mem">M</span>
+            }
+            <span id="display-text" className="display-text" data-result>
+            { props.exp && 
+              <span>{props.numCache}e</span> }
+            {props.content}</span>
+            <div className="display gradient"></div>
         </section>
     )
 }
